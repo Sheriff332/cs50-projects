cbea5b839545a8e3427d565e616c21f3f6d076de
Sheriff <66024246+Sheriffgamer332@users.noreply.github.com>
2024-03-07 17:17:16 +0530

    Update helpers.c

--- filter/filter-less/helpers.c
+++ filter/filter-less/helpers.c
@@ -1,4 +1,6 @@
 #include "helpers.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
@@ -50,7 +52,16 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
-    RGBTRIPLE copy[height][width];
+    // Allocate memory for the copy of the image
+    RGBTRIPLE (*copy)[width] = malloc(height * sizeof(RGBTRIPLE[width]));
+    if (copy == NULL)
+    {
+        // Handle memory allocation failure
+        fprintf(stderr, "Failed to allocate memory for image copy\n");
+        return;
+    }
+
+    // Copy the original image to the allocated memory
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
@@ -58,34 +69,54 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
             copy[i][j] = image[i][j];
         }
     }
+
+    // Perform the blur operation on the original image
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
-            int sumRed=0;
-            int sumBlue=0;
-            int sumGreen=0;
+            int sumRed = 0;
+            int sumBlue = 0;
+            int sumGreen = 0;
             int counter = 0;
-            for (int x=-1; x<=1; x++)
-            {
-                for (int y=-1; y<=1; y++)
-                {
-                    if ((i+x) >= 0 || (i+x) < width || (j+y) >= 0 || (j+y) < height)
-                    {
-                        sumRed+=copy[(i+x)][(j+y)].rgbtRed;
-                        sumBlue+=copy[(i+x)][(j+y)].rgbtBlue;
-                        sumGreen+=copy[(i+x)][(j+y)].rgbtGreen;
-                        counter+=1;
-                    }
 
+            // Accumulate the sum of neighboring pixel values
+            for (int y = -1; y <= 1; y++)
+            {
+                for (int x = -1; x <= 1; x++)
+                {
+                    int new_i = i + y;
+                    int new_j = j + x;
+
+                    // Check if the neighboring pixel is within the image bounds
+                    if (new_i >= 0 && new_i < height && new_j >= 0 && new_j < width)
+                    {
+                        sumRed += copy[new_i][new_j].rgbtRed;
+                        sumBlue += copy[new_i][new_j].rgbtBlue;
+                        sumGreen += copy[new_i][new_j].rgbtGreen;
+                        counter++;
+                    }
                 }
             }
-            image[i][j].rgbtRed= (int)(sumRed/counter);
-            if (image[i][j].rgbtRed > 255) {image[i][j].rgbtRed = 255;}
-            image[i][j].rgbtBlue= (int)(sumBlue/counter);
-            if (image[i][j].rgbtBlue > 255) {image[i][j].rgbtBlue = 255;}
-            image[i][j].rgbtGreen= (int)(sumGreen/counter);
-            if (image[i][j].rgbtGreen > 255) {image[i][j].rgbtGreen = 255;}
+
+            // Ensure the counter is not zero to prevent division by zero
+            if (counter == 0)
+            {
+                counter = 1;
+            }
+
+            // Calculate the average color values and update the original image
+            image[i][j].rgbtRed = (int)(sumRed / counter);
+            image[i][j].rgbtBlue = (int)(sumBlue / counter);
+            image[i][j].rgbtGreen = (int)(sumGreen / counter);
+
+            // Clamp the color values to the range [0, 255]
+            image[i][j].rgbtRed = image[i][j].rgbtRed > 255 ? 255 : image[i][j].rgbtRed;
+            image[i][j].rgbtBlue = image[i][j].rgbtBlue > 255 ? 255 : image[i][j].rgbtBlue;
+            image[i][j].rgbtGreen = image[i][j].rgbtGreen > 255 ? 255 : image[i][j].rgbtGreen;
         }
     }
+
+    // Free the allocated memory for the image copy
+    free(copy);
 }
