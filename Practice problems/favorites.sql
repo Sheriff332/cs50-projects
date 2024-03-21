b58811acc0718969e8edd14eec56fd775ce84c31
Sheriff <66024246+Sheriff332@users.noreply.github.com>
2024-03-22 00:25:49 +0530

    Create favorites.sql

---
+++ Practice problems/favorites.sql
@@ -0,0 +1,41 @@
+UPDATE shows SET title="Adventure Time" WHERE title like "Adventure Time";
+
+UPDATE shows SET title="Arrow" WHERE title like "Arrow";
+
+UPDATE shows SET title="Avatar: The Last Airbender" WHERE title like "Avatar: The Last Airbender";
+
+UPDATE shows SET title="Brooklyn Nine-Nine" WHERE title like "Brooklyn Nine-Nine";
+
+UPDATE shows SET title="Community" WHERE title like "Community";
+
+UPDATE shows SET title="Family Guy" WHERE title like "Family Guy";
+
+UPDATE shows SET title="Friends" WHERE title like "Friends";
+
+UPDATE shows SET title="Game of Thrones" WHERE title like "Game of Thrones";
+
+UPDATE shows SET title="Gilmore Girls" WHERE title like "Gilmore Girls";
+
+UPDATE shows SET title="Grey’s Anatomy" WHERE title like "Grey’s Anatomy";
+
+UPDATE shows SET title="How I Met Your Mother" WHERE title like "How I Met Your Mother";
+
+UPDATE shows SET title="It’s Always Sunny in Philadelphia" WHERE title like "It’s Always Sunny in Philadelphia";
+
+UPDATE shows SET title="Parks and Recreation" WHERE title like "Parks and Recreation";
+
+UPDATE shows SET title="Sherlock" WHERE title like "Sherlock";
+
+UPDATE shows SET title="Squid Game" WHERE title like "Squid Game";
+
+UPDATE shows SET title="The Bachelorette" WHERE title like "The Bachelorette";
+
+UPDATE shows SET title="The Crown" WHERE title like "The Crown";
+
+UPDATE shows SET title="The Office" WHERE title like "The Office";
+
+UPDATE shows SET title="The Queen’s Gambit" WHERE title like "The Queen’s Gambit";
+
+UPDATE shows SET title="The Untamed" WHERE title like "The Untamed";
+
+SELECT * FROM shows;
